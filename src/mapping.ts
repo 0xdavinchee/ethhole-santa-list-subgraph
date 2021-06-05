20fa3ed1c24d911b3e79f2c61783a5bdc2739029
Vincent Chee <0xdavinchee@gmail.com>
2021-06-05 09:37:23 +0800

    subgraph wip

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,59 +1,38 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  SantasList,
   NewNaughtyLister,
   NewNiceLister,
-  WithdrawFees
-} from "../generated/SantasList/SantasList"
-import { ExampleEntity } from "../generated/schema"
+  WithdrawFees,
+} from "../generated/SantasList/SantasList";
+import {
+  NaughtyLister,
+  NiceLister,
+  WithdrawalFeeEvent,
+} from "../generated/schema";
 
 export function handleNewNaughtyLister(event: NewNaughtyLister): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.naughtyLister = event.params.naughtyLister
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.feeAmount(...)
-  // - contract.getOwnerWithdrawableAmount(...)
-  // - contract.getWithdrawableAmount(...)
-  // - contract.naughtyList(...)
-  // - contract.niceList(...)
-  // - contract.numNiceListers(...)
-  // - contract.totalFeesAccrued(...)
+  let naughtyLister = new NaughtyLister(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  naughtyLister.address = event.params.naughtyLister;
+  naughtyLister.save();
 }
 
-export function handleNewNiceLister(event: NewNiceLister): void {}
+export function handleNewNiceLister(event: NewNiceLister): void {
+  let niceLister = new NiceLister(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  niceLister.address = event.params.niceLister;
+  niceLister.feePaid = event.params.feePaid;
 
-export function handleWithdrawFees(event: WithdrawFees): void {}
+  niceLister.save();
+}
+
+export function handleWithdrawFees(event: WithdrawFees): void {
+  let withdrawalFeeEvent = new WithdrawalFeeEvent(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  withdrawalFeeEvent.address = event.params.withdrawer;
+  withdrawalFeeEvent.amountWithdrawn = event.params.amount;
+
+  withdrawalFeeEvent.save();
+}
